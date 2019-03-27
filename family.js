613d1c7a28a13c021f631e28ef493ca61a6beae1
wolfan <wolfan@qq.com>
2019-03-27 21:21:05 +0800

    dome

---
+++ family.js
@@ -0,0 +1,15 @@
+// klano arbo
+
+// family data
+const Mapo={}
+
+// Kalkulo
+function kalkulo(arr){}
+
+// 关系距离
+function rilato(){}
+// 称呼处理
+function saluto(){}
+// 亲近
+function klano(){}
+function arbo(){}
